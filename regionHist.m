45ac02d9b649852e4e238e2d39c66b60041eed01
Evil_Lu <bigshjt.evillu@gmail.com>
2014-04-11 14:32:33 +0700

    - update regionHist.m

--- regionHist.m
+++ regionHist.m
@@ -1,10 +1,11 @@
 function hists = regionHist(imSrc,bwIm,boxxes)
     hists = zeros(size(boxxes,1),256);
     for i = 1:size(boxxes,1)
-        box = boxxes(i,1:4);
+        box = boxxes(i,:);
         partSrc = imSrc(box(2):box(4),box(1):box(3));
         partBW = bwIm(box(2):box(4),box(1):box(3));
         hists(i,:) = imhist(partBW.*partSrc);
+        hists(i:1) = 0;
     end
 end
 
